29f7d97f67a935ba5a3c42a45b08efda1f518c6d
chlalswns200 <chlalswns200@naver.com>
2023-12-26 17:58:24 +0900

    2-9 라디오 버튼

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -31,7 +31,7 @@ public class FormItemController {
         return regions;
     }
 
-    @ModelAttribute("itemType")
+    @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
         ItemType[] values = ItemType.values();
         return values;
@@ -62,6 +62,9 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}",item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+        log.info("item.itemType={}", item.getItemType());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
