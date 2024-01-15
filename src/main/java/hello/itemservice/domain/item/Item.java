2cc5a88c2ab78af0d5331a09d0a956944283e069
chlalswns200 <chlalswns200@naver.com>
2024-01-15 21:26:39 +0900

    Bean Validation - groups

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,13 +11,14 @@ import org.hibernate.validator.constraints.ScriptAssert;
 //@ScriptAssert(lang ="javascript",script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull
-    @Range(min=1000, max=1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min=1000, max=1000000,groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-    @NotNull
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     @Max(9999)
     private Integer quantity;
 
